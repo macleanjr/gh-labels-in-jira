5bfe17a874d76e8528f50740657e8fb1e5906bc4
Jason MacLean <macleajr@gmail.com>
2019-06-14 10:25:15 -0400

    adding pride feature flag

--- options.js
+++ options.js
@@ -7,13 +7,15 @@ function save_options() {
     var hide_closed_prs = document.getElementById('hide_closed_prs').checked;
     var pr_columns = document.getElementById('pr_columns').value;
     var ff_code_reviewers = document.getElementById('ff_code_reviewers').checked;
+    var ff_pride = document.getElementById('ff_pride').checked;
 
     chrome.storage.sync.set({
         github_access_token: github_access_token,
         hide_labels_on_closed_prs: hide_labels_on_closed_prs,
         hide_closed_prs: hide_closed_prs,
         pr_columns: pr_columns,
-        ff_code_reviewers: ff_code_reviewers
+        ff_code_reviewers: ff_code_reviewers,
+        ff_pride: ff_pride
     }, function () {
         // Update status to let user know options were saved.
         var status = document.getElementById('status');
@@ -32,13 +34,15 @@ function restore_options() {
         hide_labels_on_closed_prs: true,
         hide_closed_prs: false,
         pr_columns: 'In Review, Github Review',
-        ff_code_reviewers: false
+        ff_code_reviewers: false,
+        ff_pride: false
     }, function (items) {
         document.getElementById('github_access_token').value = items.github_access_token;
         document.getElementById('hide_labels_on_closed_prs').checked = items.hide_labels_on_closed_prs;
         document.getElementById('hide_closed_prs').checked = items.hide_closed_prs;
         document.getElementById('pr_columns').value = items.pr_columns;
         document.getElementById('ff_code_reviewers').checked = items.ff_code_reviewers;
+        document.getElementById('ff_pride').checked = items.ff_pride;
     });
 }
 
